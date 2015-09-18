c50645be0044690c56e997867643cfca66086294
Jiro Lin <jiro.lin9611@gmail.com>
2015-09-19 01:05:14 +0800

    rewrite the get pins and display pins method and clean the index.html
    Signed-off-by: Jiro Lin <jiro.lin9611@gmail.com>

--- assets/js/pins.js
+++ assets/js/pins.js
@@ -1,28 +1,47 @@
 /**
  * Loading the pins
  */
+
+
 var loadPins = (function() {
 
-	var pinData = '{"pinData":[
-						{"pinID":1, "pin_img":"http://cssdeck.com/uploads/media/items/1/1swi3Qy.png",
-						 "describe":"Nullam eget lectus augue. Donec eu sem sit amet ligula
-						faucibus suscipit. Suspendisse rutrum turpis quis nunc
-						convallis quis aliquam mauris suscipit."},
-						{"pinID":2, "pin_img":"http://cssdeck.com/uploads/media/items/8/8kEc1hS.png", "describe":"Donec a fermentum nisi. Integer dolor est, commodo ut
-						sagittis vitae, egestas at augue."},
-						{"pinID":3, "pin_img":"http://cssdeck.com/uploads/media/items/6/6f3nXse.png", "describe":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-						Sed feugiat consectetur pellentesque."}]}';
-
+	var pinData = '{"pinData": [{"pinID":1, "pinImg":"http://cssdeck.com/uploads/media/items/1/1swi3Qy.png","pinDetail":"https://jiro9611.com/check1.html","describe":"Nullam eget lectus augue. Donec eu sem sit amet ligula faucibus suscipit. Suspendisse rutrum turpis quis nunc convallis quis aliquam mauris suscipit."}, {"pinID":2, "pinImg":"http://cssdeck.com/uploads/media/items/1/1swi3Qy.png","pinDetail":"https://jiro9611.com/check1.html","describe":"Nullam eget lectus augue. Donec eu sem sit amet ligula faucibus suscipit. Suspendisse rutrum turpis quis nunc convallis quis aliquam mauris suscipit."}, {"pinID":3, "pinImg":"http://cssdeck.com/uploads/media/items/1/1swi3Qy.png","pinDetail":"https://jiro9611.com/check1.html","describe":"Nullam eget lectus augue. Donec eu sem sit amet ligula faucibus suscipit. Suspendisse rutrum turpis quis nunc convallis quis aliquam mauris suscipit."}]}';
 	var pinObj = JSON.parse(pinData);
 
 	return pinObj;
+
 
 })();
 
 /**
  * Display the pins
  */
- var displayPins = (funtion(){
 
- })(loadPins());
+// see design pattern p.97
+// IIFE p.91
+var displayPins = (function(getPins) {
 
+	//WAY1
+	// var pinCount = getPins.pinData.length;
+
+	// for(var i = 0; i < pinCount; i++) {
+	// 	console.log(getPins.pinData[i].pinID);
+	// 	console.log(getPins.pinData[i].pinImg);
+	// 	console.log(getPins.pinData[i].pinDetail);
+	// 	console.log(getPins.pinData[i].describe);
+	// }
+
+	//WAY2
+	var j = 0;
+	//這裡改用.each這個函式來取出JData裡的物件
+	$.each(getPins.pinData, function() {
+	  //呼叫方式也稍微改變了一下，意思等同於上述的的JData[i]["idx_Key"]
+	  $("#columns").append("<div class='pin' id='pinID_" + getPins.pinData[j].pinID + "'>" +
+	                          "<img class='pin_img' src='" + getPins.pinData[j].pinImg    + "'/>" +
+	                          // "<td>" + getPins.pinData[j].pinDetail + "</td>" +
+	                          "<p class='describe'>" + getPins.pinData[j].describe + "</p>" +
+	                          "</div>");
+	  j++;
+	});
+
+});
